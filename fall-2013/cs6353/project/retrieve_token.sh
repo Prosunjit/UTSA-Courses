a2f50dfded8cb4b78e114a847913e414fa3cee11
root <root@a8ed2c5a-f426-4cea-ac4a-6879dfc7fa2f.(none)>
2013-12-05 13:45:55 -0600

    keystone server ip changed

--- fall-2013/cs6353/project/retrieve_token.sh
+++ fall-2013/cs6353/project/retrieve_token.sh
@@ -6,7 +6,7 @@ for i in {1..1000}
 
   do
     echo 'retrieving token'
-    curl -i http://10.245.122.50:5000/v2.0/tokens -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "User-Agent: python-novaclient" -d '{"auth": {"tenantName": "demo", "passwordCredentials": {"username": "demo", "password": "admin"}}}'> token.log
+    curl -i http://10.245.121.19:5000/v2.0/tokens -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "User-Agent: python-novaclient" -d '{"auth": {"tenantName": "demo", "passwordCredentials": {"username": "demo1", "password": "admin1"}}}'> token.log
 
     X=`tail -n 1 token.log` 
     Y=`echo $X > token.log`
